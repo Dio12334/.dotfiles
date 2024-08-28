9ae844aa1cf2ab9330d853057c4e5024e51bd9dd
Dio12334 <diego.paredes.r@utec.edu.pe>
2024-08-28 16:17:42 -0500

    feat: better gotop

--- gotop/setup.sh
+++ gotop/setup.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_amd64.deb
 sudo dpkg -i gotop_v4.2.0_linux_amd64.deb
-
+rm gotop_v4.2.0_linux_amd64.db
 
 rm -rf ~/.config/gotop
 ln -s $(pwd) ~/.config/gotop
